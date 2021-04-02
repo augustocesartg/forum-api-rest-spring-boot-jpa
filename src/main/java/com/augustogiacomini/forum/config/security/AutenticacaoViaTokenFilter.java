1a295ec7e044c8c6bc26c97a19a51640d4748c36
augustogiacomini <augusto.giacomini@ciss.com.br>
2021-04-02 14:29:42 -0300

    Autenticação via JWT

--- src/main/java/com/augustogiacomini/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/com/augustogiacomini/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -1,4 +1,60 @@
 package com.augustogiacomini.forum.config.security;
 
-public class AutenticacaoViaTokenFilter {
+import com.augustogiacomini.forum.modelo.Usuario;
+import com.augustogiacomini.forum.repository.UsuarioRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    private UsuarioRepository usuarioRepository;
+
+    public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recuperarToken(httpServletRequest);
+        boolean valido = tokenService.isTokenValido(token);
+
+        if (valido) {
+            autenticarCliente(token);
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private void autenticarCliente(String token) {
+        Long idUsuario = tokenService.getIdUsuario(token);
+        Optional<Usuario> usuario = usuarioRepository.findById(idUsuario);
+
+        if (usuario.isPresent()) {
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(usuario.get(), null, usuario.get().getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+    private String recuperarToken(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader("Authorization");
+
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return token.substring(7);
+    }
 }
--- src/main/java/com/augustogiacomini/forum/controller/form/LoginForm.java
+++ src/main/java/com/augustogiacomini/forum/controller/form/LoginForm.java
@@ -1,4 +1,16 @@
 package com.augustogiacomini.forum.controller.form;
 
+import lombok.Setter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Setter
 public class LoginForm {
+
+    private String email;
+
+    private String senha;
+
+    public UsernamePasswordAuthenticationToken converter() {
+        return new UsernamePasswordAuthenticationToken(email, senha);
+    }
 }
